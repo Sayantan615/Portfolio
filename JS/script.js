83aeac56375314fff1fb7c77eea7290669757463
Sayantan Chowdhury <sayantanchowdhury615@gmail.com>
2022-06-20 19:01:46 +0530

    Changed collor choices and Contect section

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,18 @@
 // Cursor Progress bar Script
-let CursorMeter = document.getElementById('CursorMeter');
-document.addEventListener('mousemove', function(e){
-    CursorMeter.style.top = e.clientY + 'px';
-    CursorMeter.style.left = e.clientX + 'px';
-    
-})
+//Document Object Model(DOM)
+let CursorMeter = document.getElementById("CursorMeter");
+document.addEventListener("mousemove", function (e) {
+  CursorMeter.style.top = e.clientY + "px";
+  CursorMeter.style.left = e.clientX + "px";
+});
 
-let persent = document.getElementById('persent');
-let progressBar = document.getElementById('progressBar');
+let persent = document.getElementById("persent");
+let progressBar = document.getElementById("progressBar");
 
 let totalHeight = document.body.scrollHeight - window.innerHeight;
-
-window.onscroll =  function(){
-    let progress = (window.pageYOffset / totalHeight) * 100;
-    progressBar.style.width = progress + '%';
-    persent.innerHTML = 'Page Scrolled ' + Math.round(progress) + '%';
-}
-
+//** windows.scroll */
+window.onscroll = function () {
+  let progress = (window.pageYOffset / totalHeight) * 100;
+  progressBar.style.width = progress + "%";
+  persent.innerHTML = "Page Scrolled " + Math.round(progress) + "%";
+};
