c2833777501734d4fc7483c906d5632a220d2faa
Sayantan <Sayantanchowdhury615@gmail.com>
2022-07-05 14:28:08 +0530

    About section is updated and some modifications are made

--- JS/script.js
+++ JS/script.js
@@ -1,54 +1,31 @@
-// Cursor Progress bar Script
+//* Cursor Progress bar Script
 //Document Object Model(DOM)
-let CursorMeter = document.getElementById("CursorMeter");
-document.addEventListener("mousemove", function (e) {
-  CursorMeter.style.top = e.clientY + "px";
-  CursorMeter.style.left = e.clientX + "px";
-});
+// let CursorMeter = document.getElementById("CursorMeter");
+// document.addEventListener("mousemove", function (e) {
+//   CursorMeter.style.top = e.clientY + "px";
+//   CursorMeter.style.left = e.clientX + "px";
+// });
 
-let persent = document.getElementById("persent");
-let progressBar = document.getElementById("progressBar");
+// let persent = document.getElementById("persent");
+// let progressBar = document.getElementById("progressBar");
 
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-//** windows.scroll */
-window.onscroll = function () {
-  let progress = (window.pageYOffset / totalHeight) * 100;
-  progressBar.style.width = progress + "%";
-  persent.innerHTML = "Page Scrolled " + Math.round(progress) + "%";
-};
+// let totalHeight = document.body.scrollHeight - window.innerHeight;
+// //** windows.scroll */
+// window.onscroll = function () {
+//   let progress = (window.pageYOffset / totalHeight) * 100;
+//   progressBar.style.width = progress + "%";
+//   persent.innerHTML = "Page Scrolled " + Math.round(progress) + "%";
+// };
 
-const menu_toggler = document.querySelector(".menu-toggler");
-const navigation = document.querySelector(".Navigation");
-menu_toggler.onclick = function () {
-  navigation.classList.toggle("active");
-};
 
-/* Open when someone clicks on the span element */
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
+// ! There is some problem with this code solve it
+// ! add a .autoheight class in instade of the bellow code
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
-
-// ? Script for Auto-Height
-var setElementHeight = function () {
-  var height = $(window).height();
-  $(".autoheight").css("min-height", height);
-};
-
-$(window)
-  .on("resize", function () {
-    setElementHeight();
-  })
-  .resize();
-
-function initialSetup() {
-  document.getElementById("hide").style.visibility = "visible";
-  setTimeout(function () {
-    document.getElementById("hide").style.visibility = "hidden";
-  }, 8000);
-}
-initialSetup();
+// ? Script for hiding the message
+// function initialSetup() {
+//   document.getElementById("hide").style.visibility = "visible";
+//   setTimeout(function () {
+//     document.getElementById("hide").style.visibility = "hidden";
+//   }, 8000);
+// }
+// initialSetup();
