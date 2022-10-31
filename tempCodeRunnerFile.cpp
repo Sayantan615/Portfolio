e32b9899e248ed07b44747dccbb0f7e41e0f7963
sayantan <sayantanchowdhury615@gmail.com>
2022-10-31 21:05:57 +0530

    contect section full change, not good enough for android view

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class object{
+    private:
+    int val;
+    public:
+};
+int main(){
+
+    object ob;
+    cout << sizeof(ob) << endl;
+    return 0;
+}
