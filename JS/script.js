38df97ae34bb820922a36e0055f64d03fcdd7290
sayantan <sayantanchowdhury615@gmail.com>
2023-09-30 19:12:04 +0530

    working on theme and layouts

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,31 @@
 // Insitializeing AOS for div animations
 
-async function updateQuote() {
-  const quote = document.querySelector("blockquote p");
-  const cite = document.querySelector("blockquote cite");
-  // Fetch a random quote from the Quotable API
-  const response = await fetch("https://api.quotable.io/random");
-  const data = await response.json();
-  if (response.ok) {
-    // Update DOM elements
-    quote.textContent = `${data.content}`;
-    cite.textContent = `- ${data.author}`;
-  } else {
-    quote.textContent = "An error occured";
-    console.log(data);
-  }
-}
-// Random Quate Section
-document.addEventListener("DOMContentLoaded", () => {
-  // call updateQuote once when page loads
-  updateQuote();
-});
+// async function updateQuote() {
+//   const quote = document.querySelector("blockquote p");
+//   const cite = document.querySelector("blockquote cite");
+//   // Fetch a random quote from the Quotable API
+//   const response = await fetch("https://api.quotable.io/random");
+//   const data = await response.json();
+//   if (response.ok) {
+//     // Update DOM elements
+//     quote.textContent = `${data.content}`;
+//     cite.textContent = `- ${data.author}`;
+//   } else {
+//     quote.textContent = "An error occured";
+//     console.log(data);
+//   }
+// }
+// // Random Quate Section
+// document.addEventListener("DOMContentLoaded", () => {
+//   // call updateQuote once when page loads
+//   updateQuote();
+// });
 
-const randomQ = document.querySelector(".RandomQ");
+// const randomQ = document.querySelector(".RandomQ");
 
-randomQ.addEventListener("click", () => {
-  updateQuote();
-});
+// randomQ.addEventListener("click", () => {
+//   updateQuote();
+// });
 
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: "#navbarNav",
@@ -101,4 +101,4 @@ document
     }
   });
 
-changeMode();
+// changeMode();
