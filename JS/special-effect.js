a171750674c4d6ef9624fc8c591a637753c1e8da
sayantan <sayantanchowdhury615@gmail.com>
2024-03-11 20:13:13 +0530

    deleteing things

--- JS/special-effect.js
+++ JS/special-effect.js
@@ -1,8 +1,5 @@
 const scrollers = document.querySelectorAll(".scroller");
 
-// if (!window.matchMedia("(prefers-reduced-motion : reduce)").matches) {
-// }
-
 function addAnimation() {
   scrollers.forEach((scroller) => {
     scroller.setAttribute("data-animated", true);
